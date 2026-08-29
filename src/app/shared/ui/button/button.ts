42cf4c81332d2d03c976b0acb08a8b649a4ccf7a
andreialex232 <numauitnuintelegi@gmail.com>
2026-08-29 03:33:07 +0200

    feat(partials): added reusable button and clickable logo

--- src/app/shared/ui/button/button.ts
+++ src/app/shared/ui/button/button.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, input, output} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-button',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   standalone: true,
-  template: ` <p>button works!</p> `,
+  template: ` 
+    @if(element() === 'button') {
+        <button
+            [type]="buttonType()"
+            [disabled]="disabled()"
+            (click)="handleClick($event)"
+            [ngClass]="buttonStyle()"
+        >
+            <ng-content></ng-content>
+        </button>
+    }
+
+    @if(element() === 'anchor') {
+        <div class="font-dm font-semibold transition-all hover:translate-y-[2px] hover:opacity-90 active:translate-y-[3px] inline-block">
+            <a [routerLink]="url()" class="px-6 py-3 text-md cursor-pointer text-white bg-orange rounded-lg outline-none cursor-pointer border-none text-center transition-all shadow-md hover:shadow-lg">
+                <ng-content></ng-content>
+            </a>
+        </div>
+    }
+    
+  `,
   styles: ``,
 })
-export class Button {}
+export class Button {
+    element = input.required<'anchor' | 'button'>();
+    /* Buttons Inputs */
+    disabled = input<false>()
+    buttonType = input<'button' | 'submit'>();
+    buttonStyle = input<'button-sign-up' | 'normal-btn'>('normal-btn');
+    /* Anchor Inputs */
+    url = input<string>();
+    /* Outputs */
+    btnClick = output<MouseEvent>();
+
+        handleClick(event: MouseEvent): void {
+        if (!this.disabled) {
+            console.log('clicked in child')
+            this.btnClick.emit(event);
+        }
+    }
+}
