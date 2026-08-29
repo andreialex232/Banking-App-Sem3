956b12475feb767099b53779a03c3c194fb60d75
andreialex232 <numauitnuintelegi@gmail.com>
2026-08-29 03:49:52 +0200

    feat(scripts): added routes for register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { Register } from './features/auth/register/register';
 
 export const routes: Routes = [
     {
         path: '', component: Home
     },
     {
+        path: 'register', component: Register
+    },
+    {
         path: '**', redirectTo: ''
-    }
+    },
     
 ];
