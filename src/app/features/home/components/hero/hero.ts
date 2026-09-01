5066641ea29b99be87a2a81bd781a9fb2d5d6acb
andreialex232 <numauitnuintelegi@gmail.com>
2026-09-02 01:55:30 +0200

    feat(style): updating hero section with new image

--- src/app/features/home/components/hero/hero.ts
+++ src/app/features/home/components/hero/hero.ts
@@ -1,44 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { Button } from '@shared/ui/button/button';
-import { Logo } from '@shared/ui/logo/logo';
 
 @Component({
   selector: 'app-hero',
-  imports: [CommonModule, Button, Logo],
+  imports: [CommonModule, Button],
   template: `
-    <header class="text-grey relative h-screen w-screen before:absolute before:inset-0 before:-z-10 before:-scale-x-100 before:bg-cover before:bg-center before:bg-no-repeat 
-                    before:bg-[url('/assets/images/hero-small.jpg')] 
-                    md:before:bg-[url('/assets/images/hero-medium.jpg')] 
-                    lg:before:bg-[url('/assets/images/hero-huge.jpg')]">
-
-        <!-- ICON AND LOGO -->
-        <div class="absolute top-[43%] left-[61.5%] -translate-x-1/2 -translate-y-1/2">
-            <app-logo bankSize="text-xl" yourSize="text-lg" variant="vertical"></app-logo>
+<header class="grid grid-cols-12 bg-[url('/assets/images/hero-test.jpg')] bg-cover bg-no-repeat bg-center h-[70vh] sm:h-screen">
+    <div class="bg-white/80 backdrop-blur-sm p-6 rounded-xl flex mt-auto mb-auto -translate-y-25 lg:-translate-y-20 xl:-translate-y-25 2xl:-translate-y-30 self-center flex-col gap-6 col-start-2 md:col-start-7 col-end-12 lg:col-end-11">
+        <h1 class="text-3xl text-black sm:text-5xl font-bold capitalize">Banking at <span class="text-orange">your</span> fingertips</h1>
+        <div class="flex lg:text-base xl:text-lg flex-col gap-6">
+        <p class="text-grey lg:hidden font-medium text-xl">Take full control of your finances with high-yield savings and 24/7 protection.</p>
+        <p class="hidden lg:block font-medium text-xl">Simple, secure, and responsible banking designed around your life. Take full control of your finances with high-yield savings and 24/7 protection.</p>
+            <app-button url="/register" class="capitalize" element="anchor">Join today</app-button>
         </div>
-
-        <!-- LEFT SIDE TEXT -->
-        <div class="font-dm absolute top-[50%] left-[48%] -translate-x-1/2 -translate-y-1/2">
-            <h1 class="text-lg">Choose <span class="text-orange">Your Bank</span> as your bank TODAY.</h1>
-            <ul class="text-lg mt-4 mb-10">
-                <div class="pb-2">Benefits include:</div>
-                <li>
-                    Earn <span class="text-orange">points</span> on your purchases.
-                </li>
-                <li>
-                    Create and add <span class="text-orange">virtual</span> cards.
-                </li>
-                <li>
-                    Spend smartly, send quickly.
-                </li>
-            </ul>
-
-            <app-button element="anchor" url="/register">
-                Sign up now
-            </app-button>
-            
-        </div>
-    </header>
+    </div>
+</header>
   `,
 })
 export class Hero {}
