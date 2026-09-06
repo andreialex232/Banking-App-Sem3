89d4302a1b8e03498d4c4d98758d4fe4b24ac73f
andreialex232 <numauitnuintelegi@gmail.com>
2026-09-06 23:28:36 +0200

    refactor(style): patched button (will need complete redo)

--- src/app/shared/ui/button/button.ts
+++ src/app/shared/ui/button/button.ts
@@ -1,30 +1,38 @@
 import { Component, input, output} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-button',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgTemplateOutlet],
   standalone: true,
   template: ` 
     @if(element() === 'button') {
-        <button
-            [type]="buttonType()"
-            [disabled]="disabled()"
-            (click)="handleClick($event)"
-            [ngClass]="buttonStyle()"
-        >
-            <ng-content></ng-content>
-        </button>
+        <div class="font-dm font-semibold transition-all hover:translate-y-[2px] hover:opacity-90 active:translate-y-[3px] inline-block">
+            <button 
+                type="button"
+                (click)="handleClick($event)"
+                [disabled]="disabled()"
+                [type]="buttonType()"
+                class="px-6 py-3 text-md cursor-pointer text-white bg-orange rounded-lg outline-none border-none text-center transition-all shadow-md hover:shadow-lg"
+            >
+                <ng-container *ngTemplateOutlet="text"></ng-container>
+            </button>
+        </div>
     }
 
     @if(element() === 'anchor') {
         <div class="font-dm font-semibold transition-all hover:translate-y-[2px] hover:opacity-90 active:translate-y-[3px] inline-block">
             <a [routerLink]="url()" class="px-6 py-3 text-md cursor-pointer text-white bg-orange rounded-lg outline-none cursor-pointer border-none text-center transition-all shadow-md hover:shadow-lg">
-                <ng-content></ng-content>
+                <ng-container *ngTemplateOutlet="text"></ng-container>
             </a>
         </div>
     }
+
+    <ng-template #text>
+        <ng-content></ng-content>
+    </ng-template>
+    
     
   `,
   styles: ``,
@@ -33,7 +41,7 @@ export class Button {
     element = input.required<'anchor' | 'button'>();
     /* Buttons Inputs */
     disabled = input<false>()
-    buttonType = input<'button' | 'submit'>();
+    buttonType = input<'button' | 'submit'>('button');
     buttonStyle = input<'button-sign-up' | 'normal-btn'>('normal-btn');
     /* Anchor Inputs */
     url = input<string>();
@@ -41,7 +49,7 @@ export class Button {
     btnClick = output<MouseEvent>();
 
         handleClick(event: MouseEvent): void {
-        if (!this.disabled) {
+        if (!this.disabled()) {
             console.log('clicked in child')
             this.btnClick.emit(event);
         }
